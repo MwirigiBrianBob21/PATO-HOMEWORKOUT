2f287617d6eaaa3357b3d4f1be8ad820fa68b9c0
MwirigiBrianBob21 <brian.mwirigi@students.moringaschool.com>
2022-08-30 12:32:25 +0300

    Add a workouts div

--- js/index.js
+++ js/index.js
@@ -4,4 +4,30 @@ document.addEventListener("DOMContentLoaded", ()=>{
     fetch(`${apiForeign}/exerciseimage`)
     .then(response => response.json())
     .then( gallery => console.log(gallery))
+     
+    let button = document.getElementById("monday-start")
+    console.log(button)
+
+    button.addEventListener("click", ()=>{
+        console.log("Hello")
+        displayWorkoutPage()
+    })
+
+    
 })
+// how to change the divs
+function displayWorkoutPage() {
+    [].forEach.call(document.querySelectorAll('.container'), 
+        function(el){
+            if(el.style.display == 'none')
+                el.style.display = 'block';
+            else
+                
+                el.style.display = 'none'
+                // el.style.display = 'block';
+        });
+}
+
+
+
+   
