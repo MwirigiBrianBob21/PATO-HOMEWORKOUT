4e86d527893feec852643f945ce849dd0223109b
MwirigiBrianBob21 <brian.mwirigi@students.moringaschool.com>
2022-08-31 06:36:37 +0300

    Display homepage from workout session

--- js/index.js
+++ js/index.js
@@ -3,30 +3,50 @@ const apiForeign = "https://wger.de/api/v2"
 document.addEventListener("DOMContentLoaded", ()=>{
     fetch(`${apiForeign}/exerciseimage`)
     .then(response => response.json())
-    .then( gallery => console.log(gallery))
+    .then( gallery => displayExerciseImage(gallery))
      
-    let button = document.getElementById("monday-start")
-    console.log(button)
-
-    button.addEventListener("click", ()=>{
-        console.log("Hello")
-        displayWorkoutPage()
-    })
+    
+    // Adding Button Functionalities
+    Array.from(document.getElementsByClassName('button')).forEach(function (elm) { elm.addEventListener('click', displayWorkoutPage) })
+    Array.from(document.getElementsByClassName('end-button')).forEach(function (elm) { elm.addEventListener('click', displayHomePage) })
 
     
 })
+
+
 // how to change the divs
-function displayWorkoutPage() {
+function displayHomePage() {
     [].forEach.call(document.querySelectorAll('.container'), 
         function(el){
-            if(el.style.display == 'none')
-                el.style.display = 'block';
-            else
+            if(el.style.display == 'none' )
+                el.style.display = 'flex';
+            else if(el.style.display == 'flex')
                 
                 el.style.display = 'none'
                 // el.style.display = 'block';
+                else 
+                 el.style.display = 'flex'
         });
+    }
+
+
+function displayWorkoutPage() {
+    [].forEach.call(document.querySelectorAll('.container'), 
+        function(el){
+            if(el.style.display == 'flex')
+                el.style.display = 'none';
+            else if(el.style.display == 'none')
+                
+                el.style.display = 'flex'
+                
+        });
+
+       
+
+     
 }
+
+
 
 
 
